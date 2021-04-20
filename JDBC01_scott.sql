b9130d8fdfe2d4318507f95ae78045f2cb69fa5b
Harim Lee <devharimm@gmail.com>
2021-04-20 16:36:32 +0900

    DB Data input and read test with JDBC

--- JDBC01_scott.sql
+++ JDBC01_scott.sql
@@ -31,11 +31,27 @@ COMMIT;
 --○ 자바에서 Test002 클래스 실행 후 다시 확인
 SELECT *
 FROM TBL_MEMBER;
---==>> 1	김가영	010-2222-2222
+--==>> 
+/*
+2	김서현	010-2222-2222
+1	김가영	010-2222-2222
+*/
 
 
-
-
+--○ 쿼리문 준비
+SELECT SID, NAME, TEL
+FROM TBL_MEMBER
+ORDER BY 1;
+--==>>
+/*
+1	김가영	010-1111-1111
+2	김서현	010-2222-2222
+3	박정준	010-3333-3333
+4	이유림	010-4444-4444
+*/
+--> 한 줄 구성
+SELECT SID, NAME, TEL FROM TBL_MEMBER ORDER BY 1
+;
 
 
 
